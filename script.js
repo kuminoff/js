0a033375d6d044724f533fe644ab5fb069a649af
Viacheslav Kuminov <kuminov200220@gmail.com>
2022-12-05 17:36:17 +0700

    выполнил второе обязательное задание

--- script.js
+++ script.js
@@ -1,20 +1,42 @@
-const title = "Верстка";
-let screens = "Простые, Сложные, Интерактивные";
-let screenPrice = 100;
+let title = prompt('Как называется ваш проект?', 'Грильница');
+let screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные');
+let screenPrice = +prompt('Сколько будет стоить данная работа?', '12000');
+let adaptive = confirm("Нужен ли адаптив на сайте? (Ок - да, отмена - нет)");
+let service1 = prompt('Какой дополнительный тип услуги нужен?', 'Текущее обслуживание веб-сайта');
+let servicePrice1 = +prompt('Сколько это будет стоить?', '1000');
+let service2 = prompt('Какой дополнительный тип услуги нужен?', 'Поддержка WordPress');
+let servicePrice2 = +prompt('Сколько это будет стоить?', '2003');
 let rollback = 50;
-let fullPrice = 1000;
-let adaptive = true;
 
-console.log('тип title - ' + typeof title);
-console.log('тип fullPrice - ' + typeof fullPrice);
-console.log('тип adaptive - ' + typeof adaptive);
+let fullPrice = screenPrice + servicePrice1 + servicePrice2;
 
-console.log('длина строки screens - ' + screens.length);
+let servicePercentPrice = fullPrice - fullPrice * (rollback / 100);
 
-console.log('Стоимость верстки экранов ' + screenPrice + ' рублей');
-console.log('Стоимость разработки сайта ' + fullPrice + ' рублей');
+roundedServicePercentPrice = Math.ceil(servicePercentPrice); //округление в большую сторону 
+console.log(roundedServicePercentPrice);
 
-screensLower = screens.toLowerCase();
-console.log(screensLower.split(', '));
+switch (true) {
+  case fullPrice >= 30000:
+    console.log('Даем скидку в 10%');
+    break;
+  
+  case fullPrice >= 15000:
+    console.log('Даем скидку в 5%');
+    break;
+  
+  case fullPrice > 0:
+    console.log('Скидка не предусмотрена');
+    break;
+  
+  default:
+    console.log("Упс. Что-то пошло не так");
+    break;
+}
 
-console.log('Процент отката посреднику за работу ' + fullPrice * (rollback/100));
+// предудущие уроки
+// console.log('тип title - ' + typeof title + '\n' + 'тип fullPrice - ' + typeof fullPrice + '\n' + 'тип adaptive - ' + typeof adaptive);
+// console.log('длина строки screens - ' + screens.length);
+// console.log('Стоимость верстки экранов ' + screenPrice + ' рублей' + '\n' + 'Стоимость разработки сайта ' + fullPrice + ' рублей');
+// screensLower = screens.toLowerCase();
+// console.log(screensLower.split(', '));
+// console.log('Процент отката посреднику за работу ' + fullPrice * (rollback / 100));
