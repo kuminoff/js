6f3f7119aab707fc0ea4823bca3b6cc10fce4a9a
Viacheslav Kuminov <kuminov200220@gmail.com>
2022-12-06 15:08:22 +0700

    выполнил четвертое базовое задание

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-let title = prompt('Как называется ваш проект?', 'Грильница');
+"use strict";
+
+let rollback = 50;
 let screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные');
 let screenPrice = +prompt('Сколько будет стоить данная работа?', '12000');
 let adaptive = confirm("Нужен ли адаптив на сайте? (Ок - да, отмена - нет)");
@@ -6,35 +8,53 @@ let service1 = prompt('Какой дополнительный тип услуг
 let servicePrice1 = +prompt('Сколько это будет стоить?', '1000');
 let service2 = prompt('Какой дополнительный тип услуги нужен?', 'Поддержка WordPress');
 let servicePrice2 = +prompt('Сколько это будет стоить?', '2003');
-let rollback = 50;
 
-const fullPrice = screenPrice + servicePrice1 + servicePrice2;
+const showTypeOf = function (variable) {
+  console.log(variable, typeof variable);
+};
 
-const servicePercentPrice = fullPrice - fullPrice * (rollback / 100);
+const getRollbackMessage = function (price) {
+  switch (true) {
+    case price >= 3000:
+      return "Даём скидку 10%";
 
-console.log(Math.ceil(servicePercentPrice)); //округление в большую сторону    
+    case price >= 1500:
+      return "Даём скидку 5%";
 
-switch (true) {
-  case fullPrice >= 30000:
-    console.log('Даем скидку в 10%');
-    break;
-  
-  case fullPrice >= 15000:
-    console.log('Даем скидку в 5%');
-    break;
-  
-  case fullPrice > 0:
-    console.log('Скидка не предусмотрена');
-    break;
-  
-  default:
-    console.log("Упс. Что-то пошло не так");
-    break;
+    case price > 0:
+      return "Скидка не предусмотрена";
+
+    default:
+      return "Упс. Что-то пошло не так";
+  }
+};
+
+const getAllServicePrices = function (servPrice1, servPrice2) {
+  return servPrice1 + servPrice2;
+};
+
+function getFullPrice(price, servPrices) {
+    return price + servPrices;
 }
 
-console.log('тип title - ' + typeof title + '\n' + 'тип fullPrice - ' + typeof fullPrice + '\n' + 'тип adaptive - ' + typeof adaptive);
-console.log('длина строки screens - ' + screens.length);
-console.log('Стоимость верстки экранов ' + screenPrice + ' рублей' + '\n' + 'Стоимость разработки сайта ' + fullPrice + ' рублей');
-screensLower = screens.toLowerCase();
-console.log(screensLower.split(', '));
-console.log('Процент отката посреднику за работу ' + fullPrice * (rollback / 100));
+const getTitle = function (name) {
+  let nameTrans = name;
+  return name.charAt[0] = ' ' ? name.charAt(1).toUpperCase() + name.slice(2).toLowerCase() : ' ';
+};
+
+const getServicePercentPrices = function (price, roll) {
+  return price - price * (roll / 100);
+};
+
+let allServicePrices = getAllServicePrices(servicePrice1, servicePrice2);
+let fullPrice = getFullPrice(screenPrice, allServicePrices);
+let title = getTitle(prompt('Как называется ваш проект?', ' ГриЛЬНИцА'));
+let servicePercentPrice = getServicePercentPrices(fullPrice, rollback);
+
+showTypeOf(title);
+showTypeOf(fullPrice);
+showTypeOf(adaptive);
+
+console.log((screens.toLowerCase()).split(", "));
+console.log(getRollbackMessage(fullPrice));
+console.log("Итоговая стоимость за вычетом отката посреднику: " + Math.ceil(servicePercentPrice) + " рублей");
